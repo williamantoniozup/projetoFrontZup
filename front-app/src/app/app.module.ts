f11e9d6843cdcc307ed79dd1635bc9e7e14d9331
William <william.antonio@zup.com.br>
2018-12-12 10:25:20 -0200

    updates estrutura

--- front-app/src/app/app.module.ts
+++ front-app/src/app/app.module.ts
@@ -1,4 +1,8 @@
-import { PanelDatatableComponent } from './modules/panel/panel-datatable/panel-datatable.component';
+import { PanelTrashComponent } from './modules/panel/panel-trash/panel-trash.component';
+import { PanelAttendedComponent } from './modules/panel/panel-attended/panel-attended.component';
+import { PanelAllComponent } from './modules/panel/panel-all/panel-all.component';
+import { ModalComponent } from './core/components/modal/modal.component';
+import { PanelDatatableComponent } from './core/components/panel-datatable/panel-datatable.component'
 import { MenuComponent } from './modules/menu/menu.component';
 import { HeaderComponent } from './modules/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,7 +19,11 @@ import { PanelComponent } from './modules/panel/panel.component';
     HeaderComponent,
     MenuComponent,
     PanelComponent,
-    PanelDatatableComponent
+    PanelDatatableComponent,
+    ModalComponent,
+    PanelAllComponent,
+    PanelAttendedComponent,
+    PanelTrashComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-app/src/app/modules/panel/panel-all/panel-all.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-all',
+  templateUrl: './panel-all.component.html',
+  styleUrls: ['./panel-all.component.css']
+})
+export class PanelAllComponent {
+}
