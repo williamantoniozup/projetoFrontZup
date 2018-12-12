86824cd554ec8e6df045f3e4d6b2050c5576cf81
William <william.antonio@zup.com.br>
2018-12-12 11:09:26 -0200

    ambientes e models

---
+++ front-app/src/app/models/perfil.model.ts
@@ -0,0 +1,10 @@
+export class Profile {
+    public imgProfile: string
+    public name: string
+    public email: string
+    public phone: string
+    public city: string
+    public allProfile?: boolean
+    public attendedProfile?: boolean
+    public trashProfile?: boolean
+}
