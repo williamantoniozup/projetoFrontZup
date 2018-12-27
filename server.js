ff49b4891ba201808bc37010406a8f225e5776d4
William <william.antonio@zup.com.br>
2018-12-27 13:50:41 -0200

    updates 1.4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/front-app'));
+app.use(express.static(__dirname + '/dist/projetoFrontZup'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/front-app/index.html'));
+res.sendFile(path.join(__dirname + '/dist/projetoFrontZup/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
