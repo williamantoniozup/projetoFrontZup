d1599d12238e89bd87509b4c921e726cb8204e40
William <william.antonio@zup.com.br>
2018-12-28 15:31:50 -0200

    remove folder toast

--- src/app/core/components/toast-error/toast-error.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-toast-error',
-  templateUrl: './toast-error.component.html',
-  styleUrls: ['./toast-error.component.css']
-})
-export class ToastErrorComponent {    
-}
